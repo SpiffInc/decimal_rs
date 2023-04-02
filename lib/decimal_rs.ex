74548c13d871ab84ac8379d5fd71499df561f8fb
Michael Ries <riesmmm@gmail.com>
2023-04-02 10:01:34 +0100

    initial

---
+++ lib/decimal_rs.ex
@@ -0,0 +1,18 @@
+defmodule DecimalRs do
+  @moduledoc """
+  Documentation for `DecimalRs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DecimalRs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/decimal_rs_test.exs
@@ -0,0 +1,8 @@
+defmodule DecimalRsTest do
+  use ExUnit.Case
+  doctest DecimalRs
+
+  test "greets the world" do
+    assert DecimalRs.hello() == :world
+  end
+end
